2c10d6021dfa392b272c9898b5ad984943d6072e
tdronfield <tay.dronfield@gmail.com>
2023-02-14 10:49:52 -0500

    update projects

--- template-parts/projects-projects.php
+++ template-parts/projects-projects.php
@@ -1,9 +1,9 @@
 <div class="projects-container container-xxl">
     <div class="row">
 
-        <div class="col col-12 col-sm-12 col-md-6 col-lg-4">
+        <div class="col col-12 col-sm-12 col-md-6 col-lg-6">
             <div class="project-image">
-                <img src="<?php bloginfo('template_directory')?>/assets/images/chat-app.png" alt="Chat messages being exchanged between two people">
+                <img src="<?php bloginfo('template_directory')?>/assets/images/project-chat-app.jpg" alt="Chat messages being exchanged between two people">
                 <div class="overlay"></div>
             </div>
             <div class="project-link-wrapper">
@@ -12,9 +12,9 @@
             </a>
         </div>
 
-        <div class="col col-12 col-sm-12 col-md-6 col-lg-4">
+        <div class="col col-12 col-sm-12 col-md-6 col-lg-6">
             <div class="project-image">
-                <img src="<?php bloginfo('template_directory')?>/assets/images/dr-martens.png" alt="">
+                <img src="<?php bloginfo('template_directory')?>/assets/images/project-dr-martens.jpg" alt="">
                 <div class="overlay"></div>
             </div>
             <div class="project-link-wrapper">
@@ -23,46 +23,24 @@
             </a>
         </div>
 
-        <div class="col col-12 col-sm-12 col-md-6 col-lg-4">
+        <div class="col col-12 col-sm-12 col-md-6 col-lg-6">
             <div class="project-image">
-                <img src="<?php bloginfo('template_directory')?>/assets/images/todolist.png" alt="">
+                <img src="<?php bloginfo('template_directory')?>/assets/images/project-to-do-list.jpg" alt="">
                 <div class="overlay"></div>
             </div>
             <div class="project-link-wrapper">
-                <a target="_blank" href="https://to-do-list---react.glitch.me" class="project-link"><p class="title">To Do List - React</p></a>
+                <a target="_blank" href="https://to-do-list---react.glitch.me" class="project-link"><p class="title">To Do List</p></a>
             </div>
             </a>
         </div>
 
-        <div class="col col-12 col-sm-12 col-md-6 col-lg-4">
+        <div class="col col-12 col-sm-12 col-md-6 col-lg-6">
             <div class="project-image">
-                <img src="<?php bloginfo('template_directory')?>/assets/images/placeholder.jpg" alt="">
+                <img src="<?php bloginfo('template_directory')?>/assets/images/project-digital-business-card.jpg" alt="">
                 <div class="overlay"></div>
             </div>
             <div class="project-link-wrapper">
-                <a target="_blank" href="#" class="project-link"><p class="title">Title</p></a>
-            </div>
-            </a>
-        </div>
-
-        <div class="col col-12 col-sm-12 col-md-6 col-lg-4">
-            <div class="project-image">
-                <img src="<?php bloginfo('template_directory')?>/assets/images/placeholder.jpg" alt="">
-                <div class="overlay"></div>
-            </div>
-            <div class="project-link-wrapper">
-                <a target="_blank" href="#" class="project-link"><p class="title">Title</p></a>
-            </div>
-            </a>
-        </div>
-
-        <div class="col col-12 col-sm-12 col-md-6 col-lg-4">
-            <div class="project-image">
-                <img src="<?php bloginfo('template_directory')?>/assets/images/placeholder.jpg" alt="">
-                <div class="overlay"></div>
-            </div>
-            <div class="project-link-wrapper">
-                <a target="_blank" href="#" class="project-link"><p class="title">Title</p></a>
+                <a target="_blank" href="https://digi-business-card.glitch.me" class="project-link"><p class="title">Digital Business Card</p></a>
             </div>
             </a>
         </div>
